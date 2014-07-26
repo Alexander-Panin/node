e74792ce20fd1dc4c4497b0d79ec966ac6fd72dd
alex_panin <Alexander.Panin@dataart.com>
2014-07-26 10:28:09 -0700

    a

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 var http = require('http'), fs = require('fs'), Mustache = require('./mustache')
-var qs = require('querystring'), socket_io = require('socket.io')
+var qs = require('querystring'), socket_io = require('socket.io'),express = require('express')
 var redis = require("redis"), async = require('async')
 var client = redis.createClient()
+var path = require('path')
 
 //config
 var config = {}
 // make sure to change it
-config.server = "192.168.197.165"
+config.server = "192.168.188.128"
 
-var db = {}
+// init db gunction
 client.get("db", function(err, data) {
   if (data == null) {
     db.users = [{name: 'ivan', password: 'aa'}, {name: 'aa', password: 'aa'}]
@@ -23,131 +24,87 @@ client.get("db", function(err, data) {
   }
 })
 
-var actions = {
-  "/public/.+" : {
-    auth: true,
-    apply: function(res, req, params) {
-      var fname = req.url.split("/")[2] // get last param
-      var options = {encoding: 'UTF-8'}
-      fs.readFile("./public/" + fname, options, function(err, template) {
-        if (err) res.end()
-        res.writeHeader(200, {'Content-Type': 'application/octet-stream'})
-        res.end(template)
-      })
-    }
-  },
-  "/rooms/[0-9]+/file" : {
-    auth: true,
-    apply: function(res, req, params, body) {
-      body = qs.parse(body)
-      var fname = "./public/" + body.user + params[0] + body.fname
-      save_file(fname, body.data)
-      var data = {ok: true, url: fname.substring(1)}
-      res.writeHeader(200, {'Content-Type': 'application/json'})
-      res.end(JSON.stringify(data))
-    }
-  },
-  "/rooms/[0-9]+/": {
-    auth: true,
-    apply: function(res, req, params, body) {
-      render_and_resp(res, "room.html", function() {
-        var idx = params[0]
-        var room = db.rooms[idx]
-        return { title: room.name, users: room.users, idx: idx, server: config.server,
-                 messages: room.messages.map(
-                    function(x) { return x.date + '  ' + x.user + ' -- ' + x.text }),
-        }
-      })
-    }
-  },
-  "/rooms": {
-    auth: true,
-    apply: function(res, req, params) {
-      render_and_resp(res, "rooms.html", function() {
-        return {
-          rooms: db.rooms.map(function(x, i) { x.idx = i; return x; }),
-          server: config.server,
-        }
-      })
-    }
-  },
-  "/auth" : {
-    apply: function(res, req, params, body) {
-      var data = {}
-      if (check_auth(qs.parse(body))) data.ok = true
-      res.writeHeader(200, {'Content-Type': 'application/json'})
-      res.end(JSON.stringify(data))
-    }
-  },
-  "/users" : {
-    apply: function(res, req, params, body) {
-      body = qs.parse(body)
-      res.writeHeader(200, {'Content-Type': 'application/json'})
-      var us = db.users.filter(function(x) { return x.name == body.user })
-      if (us.length > 0) { res.end("{}"); return }
-      var user = { name: body.user, password: body.password }
-      db.users.push(user)
-      client.set("db", JSON.stringify(db))
-      user.ok = true
-      res.end(JSON.stringify(user))
-    }
-  },
-  "/" : {
-    apply: function(res, req, params) {
-      render_and_resp(res, 'auth.html', function() {})
-    }
-  },
-}
-
-function save_file(fname, body) {
-  console.log("save_file", body)
-  var file = fs.createWriteStream(fname)
-  file.write(body)
-}
-
-function render_and_resp(res, fname, view) {
-  var options = {encoding: 'UTF-8'}
-  fs.readFile("./templates/" + fname, options, function(err, template) {
-    res.writeHeader(200, {'Content-Type': 'text/html'})
-    res.end(Mustache.to_html(template, view()))
-  })
-}
-
-function check_auth(body) {
+function check_auth(body, db) {
   var u = body.user; p = body.password
   for (var i in db.users)
   { if (db.users[i].name == u && db.users[i].password == p) return true }
   return false
 }
 
-function parse_cookies (rc) {
-  var list = {}
-  rc && rc.split(';').forEach(function( cookie ) {
-    var parts = cookie.split('=');
-    list[parts.shift().trim()] = unescape(parts.join('='));
-  });
-  return list;
-}
+var app = express();
+// set view's dir
+app.engine('.html', require('ejs').__express)
+app.set('views', __dirname + '/templates')
+app.set('view engine', 'html')
 
-var app = http.createServer(function (req, res) {
-  var body = ""
-  req.on('data', function(data) { body+= data })
-  req.on('end', function() {
-    var key = Object.keys(actions).filter(function(x) {
-      return RegExp(x).test(req.url);
-    })[0] || '/'
-    var action = actions[key]
-    var params = req.url.match(/[0-9]+/g)
-    var cookies = parse_cookies(req.headers.cookie)
-    client.get("db", function(err, data) {
-      db = JSON.parse(data)
-      if (action.auth && !check_auth(cookies)) action = actions["/"]
-      action.apply(res, req, params, body)
+// settings
+app.use(express.json())
+app.use(express.urlencoded())
+app.use("/public", express.static(path.join(__dirname, 'public')))
+app.use(express.bodyParser({uploadDir:'./public'}))
+
+app.get('/', function(req, res) { res.render('auth', {}) })
+
+app.post('/auth', function(req, res) {
+  client.get("db", function(err, data) {
+    var res_data = {}
+    var db = JSON.parse(data)
+    console.log(req.body)
+    if (check_auth(req.body, db)) res_data.ok = true
+    res.json(res_data)
+  })
+})
+
+app.get("/rooms", function(req, res) {
+  client.get("db", function(err, data) {
+    var db = JSON.parse(data)
+    res.render('rooms', {
+      rooms: db.rooms.map(function(x, i) { x.idx = i; return x; }),
+      server: config.server,
     })
   })
-}).listen(80, '0.0.0.0')
+})
 
-var io = socket_io.listen(app)
+app.get("/rooms/:id", function(req, res) {
+  var idx = req.params.id
+  client.get("db", function(err, data) {
+    var db = JSON.parse(data)
+    var room = db.rooms[idx]
+    res.render('room', {
+      title: room.name, users: room.users, idx: idx, server: config.server,
+        messages: room.messages.map(
+          function(x) { return x.date + '  ' + x.user + ' -- ' + x.text }),
+    })
+  })
+})
+
+app.post("/users", function(req, res) {
+  client.get("db", function(err, data) {
+    var db = JSON.parse(data)
+    console.log(db.users)
+    var params = req.body
+    var us = db.users.filter(function(x) { return x.name == params.user })
+    if (us.length > 0) { res.json({ok: false, err: "already exist"}); return }
+    var user = { name: params.user, password: params.password }
+    db.users.push(user)
+    client.set("db", JSON.stringify(db))
+    user.ok = true
+    res.json(user)
+  })
+})
+
+app.post("/rooms/:id/file", function(req, res) {
+  client.get("db", function(err, data) {
+    var db = JSON.parse(data)
+    var img = req.files.img
+    var url = "/" + img.path
+    res.json({ok: true, url: url})
+  })
+})
+
+var serverApp = app.listen(80)
+var io = socket_io.listen(serverApp)
+var db = {}
 
 var messages = {
   'post_message': {
@@ -156,7 +113,8 @@ var messages = {
       var mess = {user: data.user, text: data.message, date: new Date}
       room.messages.push(mess)
       mess.room = data.room
-      io.sockets.emit("message_to_client", mess)
+      var key = "message_to_client_" + data.room
+      io.sockets.emit(key, mess)
     }
   },
   'create_room': {
