2ddca8064ade7d2bc6a296c98befebe44ca8fd13
spanin <spanin@admins-Mac-mini.local>
2014-08-23 15:29:37 +0400

    n

--- main.js
+++ main.js
@@ -27,7 +27,6 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use("/public", express.static(path.join(__dirname, 'public')))
-//app.use(express.bodyParser({uploadDir:'./public'}))
 app.use(multer({ dest:'./public/'}))
 
 app.get('/', function(req, res) { res.render('auth', {}) })
